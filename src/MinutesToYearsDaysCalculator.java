7ea7a71ad15dcd994482678421c3b23f58e911e9
Anand Satheesh <anands282@gmail.com>
2024-05-24 20:18:08 +0530

    updated classes

---
+++ src/MinutesToYearsDaysCalculator.java
@@ -0,0 +1,13 @@
+public class MinutesToYearDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        if (minutes <0){
+            System.out.println("Invalid Value");
+        }
+        else
+        {
+            int years = (int)(minutes/525600);
+            int days =(int) (minutes % 525600)/1440;
+            System.out.println(minutes + " min = " + years + " y and " + days + " d");
+        }
+    }
+}
