0ecf179442b965b2d7145e3e5170c5072a2c9ef1
Anand Satheesh <anands282@gmail.com>
2024-05-25 10:21:54 +0530

    added last digit checker

--- src/LastDigitChecker.java
+++ src/LastDigitChecker.java
@@ -1,2 +1,21 @@
 public class LastDigitChecker {
+    public static boolean hasSameLastDigit(int x, int y, int z){
+        if(isValid(x) && isValid(y) && isValid(z)){
+            int ldx = x % 10;
+            int ldy = y % 10;
+            int ldz = z % 10;
+
+            if (ldx != ldy && ldy != ldz && ldx != ldz)
+                return false;
+            else
+                return true;
+        }
+        else
+            return false;
+    }
+    public static boolean isValid(int num){
+        if(num<10 || num > 1000)
+            return false;
+        return true;
+    }
 }
