3a092109903afbe33aef2aa65c00266656a3c966
Anand Satheesh <anands282@gmail.com>
2024-05-25 19:06:08 +0530

    updated

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,3 +1,15 @@
 public class PerfectNumber {
-    //code
+    public static boolean isPerfectNumber(int number){
+        if(number <1)
+            return false;
+        int sum = 0;
+        for(int i = 1;i<=number/2;i++) {
+            if (number % i == 0)
+                sum += i;
+        }
+        if(sum == number)
+            return true;
+        else
+            return false;
+    }
 }
