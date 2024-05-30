5c4852fba1d507580e6eef4bae454509dbbd4051
Anand Satheesh <anands282@gmail.com>
2024-05-30 12:34:29 +0530

    updated the exercice 38

--- src/Printer.java
+++ src/Printer.java
@@ -3,6 +3,12 @@ public class Printer {
     private int pagesPrinted;
     private boolean duplex;
 
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = (tonerLevel>=0 && tonerLevel<=100)? tonerLevel:-1;
+        this.pagesPrinted = 0;
+        this.duplex = duplex;
+    }
+
     public int addToner(int tonerAmount){
         int tempCheck = tonerLevel + tonerAmount;
         if(tempCheck <0 || tempCheck>100)
@@ -14,11 +20,15 @@ public class Printer {
     public int printPages(int pagesToPrint){
         int sheetsOfPaper = pagesToPrint;
         if(duplex){
-            sheetsOfPaper = (int)Math.ceil(pagesToPrint/2);
+            sheetsOfPaper = (int)Math.ceil(pagesToPrint/2.0);
             System.out.println("Duplex Printer");
         }
         pagesPrinted +=sheetsOfPaper;
         return sheetsOfPaper;
 
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
     }
 }
