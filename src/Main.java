17075aa356b3cdf86063b1edb578c422a4759263
Anand Satheesh <anands282@gmail.com>
2024-05-30 10:22:36 +0530

    updated the exercice 37

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,15 @@ public class Main {
 //        InputCalculator.inputThenPrintSumAndAverage();
         SmartKitchen kitchen = new SmartKitchen();
 
-        kitchen.getBrewMaster().setHasWorkToDo(true);
-        kitchen.getDishWasher().setHasWorkToDo(true);
-        kitchen.getIceBox().setHasWorkToDo(true);
-
-        kitchen.getBrewMaster().brewCoffee();
-        kitchen.getDishWasher().doDishes();
-        kitchen.getIceBox().orderFood();
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//
+//        kitchen.getBrewMaster().brewCoffee();
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getIceBox().orderFood();
+        kitchen.setKitchenState(true, false, false);
+        kitchen.doKitchenWork();
 
     }
 }
