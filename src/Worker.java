414ae38727c7bbae7b45b547e0bb1a80e40f1506
Anand Satheesh <anands282@gmail.com>
2024-05-31 09:06:36 +0530

    updated worker methods

--- src/Worker.java
+++ src/Worker.java
@@ -3,19 +3,25 @@ public class Worker {
     private String birthDate;
     private String endDate;
 
+    public Worker(){
+
+    }
+
     public Worker(String name, String birthDate) {
         this.name = name;
         this.birthDate = birthDate;
     }
 
     public int getAge() {
-        return birthDate;
+        int currentYear = 2024;
+        int birthYear = Integer.parseInt(birthDate.substring(6));
+        return (currentYear - birthYear);
     }
 
     public double collectPay(){
-
+        return 0.0;
     }
     public void terminate(String endDate){
-
+        this.endDate = endDate;
     }
 }
